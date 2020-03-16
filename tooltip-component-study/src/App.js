3b0d260a29d8f0859fd324091bafdb0bfe62ff7a
John Azzaro <jazzaro@temple.edu>
2020-03-16 16:30:55 -0700

    updated component test

--- tooltip-component-study/src/App.js
+++ tooltip-component-study/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Split from './composition/Split';
 
 function App() {
   return(
     <main className='App'>
     Testing
+    < Split />
+
   </main>
   );
 }
---
+++ tooltip-component-study/src/composition/Split.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Split() {
+  return(
+    <p>This is the content of the split component</p>
+  );
+}
+
+
+export default Split;
