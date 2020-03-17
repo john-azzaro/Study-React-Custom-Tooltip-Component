c3acf963dd8fe5d6e1f8090b71c9629d4fe4fbb6
John Azzaro <jazzaro@temple.edu>
2020-03-16 17:42:41 -0700

    updated App.js, App.css, workflow

--- tooltip-component-study/src/App.js
+++ tooltip-component-study/src/App.js
@@ -5,8 +5,13 @@ import './App.css';
 function App() {
   return(
     <main className='App'>
-      <Split />                 
-    </main>
+    <Split>
+      This is the content for the left `Split`. Lorem ipsum etc.
+    </Split>
+    <Split>
+      This is the content for the right `Split`. Inventore a etc.
+    </Split>
+  </main>
   );
 }
 
